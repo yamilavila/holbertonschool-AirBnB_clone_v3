eef7444b96f15a80b32f9a5ec1aac6e067c6164f
yamilavila <yamilpra@hotmail.com>
2022-09-20 17:24:36 -0400

    Debuging

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,6 +10,7 @@ from models import storage
 classes_rel = {"Amenity": "amenities", "City": "cities", "Place": "places",
                "Review": "reviews", "State": "states", "User": "users"}
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ returns a JSON """
@@ -25,5 +26,4 @@ def stats():
     for key, value in classes_rel.items():
         number = storage.count(key)
         obj_count[value] = number
-    
     return jsonify(stat)
