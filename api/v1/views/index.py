8d01aece6553327e7191dabcbc8e04cb0ac60314
yamilavila <yamilpra@hotmail.com>
2022-09-20 19:31:31 -0400

    Fixing models and routes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,16 @@
 
 
 from api.v1.views import app_views
-from flask import app_views
+from flask import jsonify
 from models import storage
-
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 classes_rel = {"Amenity": "amenities", "City": "cities", "Place": "places",
                "Review": "reviews", "State": "states", "User": "users"}
@@ -14,16 +21,18 @@ classes_rel = {"Amenity": "amenities", "City": "cities", "Place": "places",
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ returns a JSON """
-    response = {'status': 'OK'}
+    response = {'status':'OK'}
     return jsonify(response)
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    """Retrieves the number of each objects by type"""
-    obj_count = {}
-
-    for key, value in classes_rel.items():
-        number = storage.count(key)
-        obj_count[value] = number
+    """ number of each objects by type"""
+    response = jsonify(amenties=storage.count(Amenity),
+                       cities=storage.count(City),
+                       places=storage.count(Place),
+                       reviews=storage.count(Review),
+                       states=storage.count(State),
+                       users=storage.count(User))
+    
     return jsonify(stat)
